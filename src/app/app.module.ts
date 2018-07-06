9dc2680ff8709c0f8a13958dff15a3c1dca99049
AUSTRALIA\Prakash.Paudel <prakash.paudel@civica.com.au>
2018-07-06 21:47:44 +1000

    added few pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { CategoryTilesComponent } from './category-tiles/category-tiles.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    CategoryTilesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
